649be3d1624665ce5276d40399c8e2fee6226681
huybui13 <43043174+huybui13@users.noreply.github.com>
2018-10-06 12:30:50 -0400

    New file fixed
    The files have been fixed and work now.

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -12,19 +12,29 @@ public class StringUtils {
             Ex: firstName = "Sean", lastName = "Flannery"
                     --> res = "SFlannery"
          */
-        String res = firstName.substring(0,1) + lastName;
+        String res = "";
+        if (lastName.length()>8){
+             res = firstName.substring(0,1) + lastName.substring(0,8);
+
         /* Shorten the string to 8 characters
             Ex: res = "SFlannery"
                     --> res = "SFlanner"
          */
-        res.substring(0, 8);
+             res=res.substring(0, 8);
         /* Convert the name to lower-case
             Ex: res = "SFlanner"
                     --> res = "sflanner"
          */
-        res.toLowerCase();
+        res=res.toLowerCase();
         /* return the result of our calculation */
+
+        } else if (lastName.length()<=8){
+             res = firstName.substring(0,1) + lastName.substring(0,lastName.length());
+             res=res.toLowerCase();
+
+        }
         return res;
+
     }
 
     /**
@@ -36,6 +46,7 @@ public class StringUtils {
      */
     public static String replaceStudentUsername(String text, String username) {
         /* Replace every instance of the username with proper message */
-        return text.replaceAll(username, "[DATA EXPUNDGED]");
+        text = text.replaceAll(username, "[DATA EXPUNGED]");
+        return text;
     }
 }
